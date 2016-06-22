aef00ada06637dc4a602b7a38bbbde35002c9dd9
Douglas Lamb <douglaslamb@douglaslamb.com>
2016-06-22 16:31:56 -0500

    first

---
+++ keyboard-201606220-01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  keyboard-201606220-01
+//
+//  Created by rocker on 20160622.
+//  Copyright © 2016 rocker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ keyboard/ModestUIButton.swift
@@ -0,0 +1,21 @@
+//
+//  ModestUIButton.swift
+//  keyboard-201606220-01
+//
+//  Created by rocker on 20160622.
+//  Copyright © 2016 rocker. All rights reserved.
+//
+
+import UIKit
+
+class ModestUIButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
