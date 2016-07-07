874c37f7a7b5efdc407ea3faac9122c08c2fc78a
Douglas Lamb <douglaslamb@douglaslamb.com>
2016-07-06 23:16:57 -0500

    created util row at bottom and added width constraints to all keys on that row except spacebar because spacebar does not need width constraint

--- keyboard/KeyboardViewController.swift
+++ keyboard/KeyboardViewController.swift
@@ -14,6 +14,7 @@ class KeyboardViewController: UIInputViewController {
     var topRowView: UIView = UIView()
     var midRowView: UIView = UIView()
     var bottomRowView: UIView = UIView()
+    var utilRowView: UIView = UIView()
 
     @IBAction func printChar(sender: ModestUIButton) {
         self.textDocumentProxy.insertText(sender.titleLabel!.text!)
@@ -80,7 +81,7 @@ class KeyboardViewController: UIInputViewController {
         let bottomRowButtonTitles = ["Z", "X", "C", "V", "B", "N", "M"]
         bottomRowButtons = bottomRowButtons + createButtons(bottomRowButtonTitles)
         
-        // add backspace to bottom row
+        // add backspace key to bottom row
         
         let backspaceImage = UIImage(named: "backspace")
         let backspaceKey = UIImageView(image: backspaceImage)
@@ -101,32 +102,85 @@ class KeyboardViewController: UIInputViewController {
         self.view.addSubview(bottomRowView)
         self.bottomRowView = bottomRowView
         
-        /*
+        
         // create util row buttons
-        let utilRowButtonTitles = ["Z", "X", "C", "V", "B", "N", "M"]
         
         var utilRowButtons = [UIView]()
         
+        // numbers key
         
-        bottomRowButtons = bottomRowButtons + createButtons(bottomRowButtonTitles)
-        var bottomRowView = UIView(frame: CGRectMake(0, 80, 320, 40))
-        bottomRowView.backgroundColor = UIColor.lightGrayColor()
+        let numbersKey = UIView()
+        let numbersKeyLabel = UILabel(frame: CGRectMake(10.0, 10.0, 60, 25))
+        numbersKeyLabel.text = "123"
+        numbersKey.addSubview(numbersKeyLabel)
         
-        for button in bottomRowButtons {
-            bottomRowView.addSubview(button)
+        numbersKey.backgroundColor = UIColor.whiteColor()
+        numbersKey.layer.cornerRadius = 5
+        numbersKey.translatesAutoresizingMaskIntoConstraints = false
+        
+        utilRowButtons.append(numbersKey)
+
+        // next keyboard key
+        
+        let nextKeyboardImage = UIImage(named: "nextKeyboard")
+        let nextKeyboardKey = UIImageView(image: nextKeyboardImage)
+        
+        nextKeyboardKey.backgroundColor = UIColor.whiteColor()
+        nextKeyboardKey.layer.cornerRadius = 5
+        nextKeyboardKey.translatesAutoresizingMaskIntoConstraints = false
+        nextKeyboardKey.layer.masksToBounds = true
+        
+        utilRowButtons.append(nextKeyboardKey)
+        
+        // spacebar
+        
+        let spacebarKey = UIView()
+        
+        spacebarKey.backgroundColor = UIColor.whiteColor()
+        spacebarKey.layer.cornerRadius = 5
+        spacebarKey.translatesAutoresizingMaskIntoConstraints = false
+        
+        utilRowButtons.append(spacebarKey)
+        
+        // return key
+        
+        let returnImage = UIImage(named: "return")
+        let returnKey = UIImageView(image: returnImage)
+        
+        returnKey.backgroundColor = UIColor.whiteColor()
+        returnKey.layer.cornerRadius = 5
+        returnKey.translatesAutoresizingMaskIntoConstraints = false
+        returnKey.layer.masksToBounds = true
+        
+        utilRowButtons.append(returnKey)
+        
+        // add util buttons to util view
+        
+        let utilRowView = UIView(frame: CGRectMake(0, 120, 320, 40))
+        utilRowView.backgroundColor = UIColor.lightGrayColor()
+        
+        for button in utilRowButtons {
+            utilRowView.addSubview(button)
         }
         
-        self.view.addSubview(bottomRowView)
-        self.bottomRowView = bottomRowView
-         */
-
+        self.view.addSubview(utilRowView)
+        self.utilRowView = utilRowView
+        
+        // add util button constraints
+        
+        // add numbers button constraint
+        
+        // add nextKeyboard button constraint
+        
+        // add return button constraint
         
         // add constraints
  
         addConstraints(topRowButtons, containingView: topRowView)
         addConstraints(midRowButtons, containingView: midRowView)
         addConstraints(bottomRowButtons, containingView: bottomRowView)
-    
+        addUtilRowConstraints(utilRowButtons, containingView: utilRowView)
+     
         // Perform custom UI setup here
         self.nextKeyboardButton = UIButton(type: .System)
         self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
@@ -191,10 +245,56 @@ class KeyboardViewController: UIInputViewController {
         }
     }
     
+    func addUtilRowConstraints(buttons: [UIView], containingView: UIView) {
+        for (index, button) in buttons.enumerate() {
+            
+            var topConstraint = NSLayoutConstraint(item: button, attribute: .Top, relatedBy: .Equal, toItem: containingView, attribute: .Top, multiplier: 1.0, constant: 1)
+            var bottomConstraint = NSLayoutConstraint(item: button, attribute: .Bottom, relatedBy: .Equal, toItem: containingView, attribute: .Bottom, multiplier: 1.0, constant: -1)
+            var leftConstraint : NSLayoutConstraint!
+            
+            switch index {
+            case 0:
+                // numbers key
+                leftConstraint = NSLayoutConstraint(item: button, attribute: .Left, relatedBy: .Equal, toItem: containingView, attribute: .Left, multiplier: 1.0, constant: 1)
+                var widthConstraint = NSLayoutConstraint(item: button, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 40)
+                containingView.addConstraint(widthConstraint)
+                
+            case 1:
+                // next keyboard key
+                leftConstraint = NSLayoutConstraint(item: button, attribute: .Left, relatedBy: .Equal, toItem: buttons[index - 1], attribute: .Right, multiplier: 1.0, constant: 1)
+                var widthConstraint = NSLayoutConstraint(item: buttons[0], attribute: .Width, relatedBy: .Equal, toItem: button, attribute: .Width, multiplier: 1.0, constant: 0)
+                containingView.addConstraint(widthConstraint)
+                
+            case 2:
+                // spacebar
+                leftConstraint = NSLayoutConstraint(item: button, attribute: .Left, relatedBy: .Equal, toItem: buttons[index - 1], attribute: .Right, multiplier: 1.0, constant: 1)
+                
+            case 3:
+                // return key
+                leftConstraint = NSLayoutConstraint(item: button, attribute: .Left, relatedBy: .Equal, toItem: buttons[index - 1], attribute: .Right, multiplier: 1.0, constant: 1)
+                var widthConstraint = NSLayoutConstraint(item: button, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 80)
+                containingView.addConstraint(widthConstraint)
+                
+            default:
+                print()
+                
+            }
+            
+            var rightConstraint : NSLayoutConstraint!
+            
+            if index == buttons.count - 1 {
+                rightConstraint = NSLayoutConstraint(item: button, attribute: .Right, relatedBy: .Equal, toItem: containingView, attribute: .Right, multiplier: 1.0, constant: -1)
+            } else {
+                rightConstraint = NSLayoutConstraint(item: button, attribute: .Right, relatedBy: .Equal, toItem: buttons[index + 1], attribute: .Left, multiplier: 1.0, constant: -1)
+            }
+            
+            containingView.addConstraints([topConstraint, bottomConstraint, rightConstraint, leftConstraint])
+        }
+    }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         print("touchesMoved" + String(arc4random_uniform(9)))
-        let subviews = self.topRowView.subviews + self.midRowView.subviews + self.bottomRowView.subviews
+        let subviews = self.topRowView.subviews + self.midRowView.subviews + self.bottomRowView.subviews + self.utilRowView.subviews
         var isTouchInButton = false
         //print(subviews.count)
         for subview in subviews {
@@ -215,8 +315,8 @@ class KeyboardViewController: UIInputViewController {
     }
     
     func handleTouchMoveInButton(view: UIView) {
-        var buttonLabel = view.subviews[0] as! UILabel
-        var currButtonLabel = buttonLabel.text!
+        let buttonLabel = view.subviews[0] as! UILabel
+        let currButtonLabel = buttonLabel.text!
         //checking if we delete text
         // or insert or both
         if (self.prevButton != currButtonLabel) {
@@ -241,7 +341,7 @@ class KeyboardViewController: UIInputViewController {
         print("touchesBegan" + String(arc4random_uniform(9)))
         
         // gather subviews
-        let subviews = self.topRowView.subviews + self.midRowView.subviews + self.bottomRowView.subviews
+        let subviews = self.topRowView.subviews + self.midRowView.subviews + self.bottomRowView.subviews + self.utilRowView.subviews
         var isTouchInButton = false
         
         // check each uiview for touch
