78122ba1af657bac39bb1b3149046e21a58d5cc9
Douglas Lamb <douglaslamb@douglaslamb.com>
2016-07-18 17:14:34 -0500

    started constraintmaker class

---
+++ ConstraintMaker.swift
@@ -0,0 +1,21 @@
+//
+//  ConstraintMaker.swift
+//  keyboard-201606220-01
+//
+//  Created by rocker on 20160718.
+//  Copyright © 2016 rocker. All rights reserved.
+//
+
+import UIKit
+
+class ConstraintMaker {
+    
+    static func addButtonConstraintsToRow() {
+        
+    }
+    
+    static func addRowConstraintsToSuperview() {
+        
+    }
+    
+}
