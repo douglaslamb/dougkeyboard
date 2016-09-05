7aa2aaf916e0dc7184b70c9fdc32c94b317d0b31
Douglas Lamb <douglaslamb@douglaslamb.com>
2016-09-05 06:31:22 -0500

    started the app page

--- keyboard-201606220-01/ViewController.swift
+++ keyboard-201606220-01/ViewController.swift
@@ -12,6 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let logo = UIImageView(image: UIImage(named: "spaceKeyLogo"))
+        view.addSubview(logo)
+        
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        logo.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+        logo.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor, constant: -200).active = true
+        
+        let text = UILabel()
+        view.addSubview(text)
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+        text.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor).active = true
+        text.text = "Hello"
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
